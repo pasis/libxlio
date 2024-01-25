c3e706273ec516e01798d80b3349a3e1694f7122
Dmytro Podgornyi <dmytrop@nvidia.com>
2024-01-25 04:59:14 +0200

    Always allow flush from zerocopy completion callback
    Signed-off-by: Dmytro Podgornyi <dmytrop@nvidia.com>

--- src/core/lwip/tcp_out.c
+++ src/core/lwip/tcp_out.c
@@ -1744,9 +1744,11 @@ err_t tcp_output(struct tcp_pcb *pcb)
        code. If so, we do not output anything. Instead, we rely on the
        input processing code to call us when input processing is done
        with. */
+/* XXX Allow to flush pending data from zerocopy completion callback which is called from RX path
     if (pcb->is_in_input) {
         return ERR_OK;
     }
+*/
 
     wnd = LWIP_MIN(pcb->snd_wnd, pcb->cwnd);
 
